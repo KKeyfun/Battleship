3bacb531955f76ee725b221b7abd4f0179dd3a7b
kkeyfun <KKeyfun@users.noreply.github.com>
2023-04-19 23:23:56 -0700

    Added functionality to reset the game

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import createPlayer from './modules/createPlayer';
-import { placeShips, placeShipsRobot } from './modules/placeShip';
-import startGame from './gameController';
+import { placeShips, placeShipsAuto } from './modules/placeShip';
+import { startGame } from './modules/gameController';
 
 const gamePieces = [
   ['Carrier', 5],
@@ -11,16 +11,35 @@ const gamePieces = [
   ['Patrol Boat', 2],
 ];
 
+// Player places all ships, then game begins
 async function setupGame() {
   const playerOne = createPlayer(gamePieces, 'p1');
   await placeShips(playerOne);
+  // placeShipsAuto(playerOne); // debug
   document.querySelector('.rotate').style.display = 'none';
-  console.log(playerOne.gameBoard.board);
   const playerRobot = createPlayer(gamePieces, 'robot');
   document.querySelector('.playerTwo').style.display = 'flex';
-  placeShipsRobot(playerRobot);
-  console.log(playerOne, playerRobot);
+  placeShipsAuto(playerRobot);
   startGame(playerOne, playerRobot);
 }
 
+// Clears all tile styling, empties message box, and begins setup
+function resetGame() {
+  document.querySelector('.rotate').style.display = 'block';
+  const classes = ['ship', 'hit', 'miss'];
+  document.querySelectorAll('.tile').forEach((tile) => {
+    classes.forEach((e) => {
+      tile.classList.remove(e);
+    });
+  });
+  document.querySelector('.playerTwo').style.display = 'none';
+  document.querySelector('.overlay').style.display = 'none';
+  const messageBox = document.querySelector('.messageContainer');
+  while (messageBox.children.length > 0) {
+    messageBox.removeChild(messageBox.firstElementChild);
+  }
+  setupGame();
+}
+
+document.querySelector('.reset').onclick = resetGame;
 setupGame();
