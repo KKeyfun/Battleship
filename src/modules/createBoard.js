0ed98881c64458e0f08462ad69003f6559fab700
kkeyfun <KKeyfun@users.noreply.github.com>
2023-04-19 23:26:02 -0700

    Changed receiveAttack returns

--- src/modules/createBoard.js
+++ src/modules/createBoard.js
@@ -5,7 +5,7 @@ function createBoard(player) {
   for (let y = 0; y < 10; y++) {
     const currentRow = (player === 'p1') ? document.querySelectorAll(`.playerOne [data-y="${y}"]`) : document.querySelectorAll(`.playerTwo [data-y="${y}"]`);
     for (let x = 0; x < 10; x++) {
-      board[x][y] = tile(currentRow[x]); // maybe also do a tile object representing hits
+      board[x][y] = tile(currentRow[x]);
     }
   }
   return {
@@ -13,14 +13,17 @@ function createBoard(player) {
     hitCoordinates: [],
     receiveAttack(x, y) {
       const tileObj = this.board[x][y];
+      this.hitCoordinates.push([x, y]);
       if (tileObj.isOccupied) {
-        tileObj.ship.hit();
-        tileObj.markHit();
-        this.hitCoordinates.push([x, y]);
-        return 'hit';
+        tileObj.ship.hit(); // update ship hit counter
+        tileObj.markHit(); // add styling to tile
+        if (tileObj.ship.isSunk()) {
+          return tileObj.ship;
+        }
+        return true;
       }
       tileObj.markMiss();
-      return 'missed';
+      return false;
     },
   };
 }
